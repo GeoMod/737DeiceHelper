4ecdfe7f4de0d7647d05270b975d805388b0b3c0
Dan O'Leary <olearydaniel@me.com>
2021-02-03 08:42:14 -0600

    Commented out code deleted.

--- Deice737 WatchKit Extension/ComplicationController.swift
+++ Deice737 WatchKit Extension/ComplicationController.swift
@@ -40,7 +40,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 
 	func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
 		// Call the handler with the current timeline entry
-//		let supportedComplications: [CLKComplicationFamily] = [.circularSmall, .graphicBezel, .graphicCorner, .graphicCircular, .modularSmall, .modularLarge, .utilitarianSmall, .utilitarianLarge,.utilitarianSmallFlat]
 		let now = Date()
 
 		switch complication.family {
@@ -83,7 +82,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 			default:
 				handler(nil)
 		}
-//        handler(nil)
 	}
 
 	func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -154,51 +152,34 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 	}
 
 	// MARK: - Templates for Selection and Lockscreen Display
+	// Since Templates and Timeline entries are the same, the complication only launches the watchOS App, therefore `createTimelineEntry(for:)` is used again here.
 	func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
 		// This method will be called once per supported complication, and the results will be cached
-		let sampleText = "Deice 737?"
 		switch complication.family {
 			case .circularSmall:
 				let entry = createTimelineEntry(for: complication)
-				let image = UIImage(named: "Complication/Circular")!
-				let imageProvider = CLKImageProvider(onePieceImage: image)
-				let circularSmall = CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: imageProvider )
 				handler(entry)
 			case .graphicBezel:
-				let image = UIImage(named: "Complication/Graphic Bezel")!
-				let fullColorImage = CLKFullColorImageProvider(fullColorImage: image)
-				let graphicCircularImageTemplate = CLKComplicationTemplateGraphicCircularImage(imageProvider: fullColorImage)
-				let text = CLKSimpleTextProvider(text: sampleText)
-				let graphicBezelTemplate = CLKComplicationTemplateGraphicBezelCircularText(circularTemplate: graphicCircularImageTemplate, textProvider: text)
-				handler(graphicBezelTemplate)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			case .graphicCorner:
-				let image = UIImage(named: "Complication/Graphic Corner")!
-				let fullColorImageProvider = CLKFullColorImageProvider(fullColorImage: image)
-				let text = CLKSimpleTextProvider(text: sampleText)
-				let graphicCorner = CLKComplicationTemplateGraphicCornerTextImage(textProvider: text, imageProvider: fullColorImageProvider)
-				handler(graphicCorner)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			case .modularSmall:
-				let image = UIImage(named: "Complication/Modular")!
-				let modularSmall = CLKComplicationTemplateModularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: image))
-				handler(modularSmall)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			case .modularLarge:
-				let header = CLKSimpleTextProvider(text: sampleText)
-				let body1 = CLKSimpleTextProvider(text: "Safety Is No Accident!")
-				let modularLarge = CLKComplicationTemplateModularLargeStandardBody(headerTextProvider: header, body1TextProvider: body1)
-				handler(modularLarge)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			case .utilitarianSmall:
-				let image = UIImage(named: "Complication/Utilitarian")!
-				let imageProvider = CLKImageProvider(onePieceImage: image)
-				let utilitarianSmall = CLKComplicationTemplateUtilitarianSmallSquare(imageProvider: imageProvider)
-				handler(utilitarianSmall)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			case .utilitarianSmallFlat:
-				let text = CLKSimpleTextProvider(text: sampleText, shortText: "737")
-				let utilitarianSmallFalt = CLKComplicationTemplateUtilitarianSmallFlat(textProvider: text)
-				handler(utilitarianSmallFalt)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			case .utilitarianLarge:
-				let text = CLKSimpleTextProvider(text: sampleText, shortText: "737")
-				let utilitarianLarge = CLKComplicationTemplateUtilitarianLargeFlat(textProvider: text)
-				handler(utilitarianLarge)
+				let entry = createTimelineEntry(for: complication)
+				handler(entry)
 			default:
 				handler(nil)
 		}
