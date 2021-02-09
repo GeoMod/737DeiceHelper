8d0b58fbf48602475759e70a1fd8f05f49f0c086
Dan O'Leary <olearydaniel@me.com>
2021-02-09 13:16:42 -0800

    Smartened up the code a bit

--- Deice737/ContentView.swift
+++ Deice737/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
 						.fontWeight(.bold)
 						.foregroundColor(.red)
 						.minimumScaleFactor(0.8)
+						.padding([.leading, .trailing])
 					HStack {
 						Text("Is frost OUTSIDE the CSFF area on top of the wing?")
 							.minimumScaleFactor(0.8)
@@ -35,18 +36,13 @@ struct ContentView: View {
 							frostOutsideCSFF = true
 							evaluate()
 						}, label: {
-							Text(frostOutsideCSFF ? "Yes" : "No")
+							YesNoButtonView(title: frostOutsideCSFF ? "Yes" : "No", condition: frostOutsideCSFF)
 						})
-						.frame(width: 100, height: 50)
-						.background(RoundedRectangle(cornerRadius: 30).fill(frostOutsideCSFF ? Color.green : Color.red))
-						.foregroundColor(.white)
 					}
 					VStack(alignment: .leading) {
 						HStack {
 							Image(systemName: "arrow.triangle.2.circlepath")
 								.opacity(0.25)
-								.shadow(color: Color.black.opacity(0.6), radius: 1, x: 2, y: 3)
-								.padding(.trailing)
 							Button(action: {
 								upperWingClean.toggle()
 							}, label: {
@@ -54,73 +50,21 @@ struct ContentView: View {
 									.foregroundColor(upperWingClean ? .green : .orange)
 									.font(.system(size: 30, weight: .bold))
 									.shadow(color: Color.black.opacity(0.6), radius: 7, x: 5, y: 5)
-
 							})
-							Spacer()
 						}
 					}
 				}.padding(.top, 3)
 
-				Group {
-					HStack {
-						Image(systemName:outsideAirTemp ? "thermometer.sun.fill" : "thermometer.snowflake")
-							.renderingMode(.original)
-							.font(.largeTitle)
-						Text("Is OAT greater than or equal to +4ºC?")
-							.minimumScaleFactor(0.8)
-						Spacer()
-						Button(action: {
-							outsideAirTemp.toggle()
-						}, label: {
-							Text(outsideAirTemp ? "Yes" : "No")
-						})
-						.frame(width: 100, height: 50, alignment: .center)
-						.background(RoundedRectangle(cornerRadius: 30).fill(outsideAirTemp ? Color.green : Color.red))
-						.foregroundColor(.white)
-					}
-					HStack {
-						Image(systemName:fuelTemp ? "airplane.circle.fill" : "drop.triangle.fill")
-							.renderingMode(.original)
-							.font(.largeTitle)
-						Text("Is Fuel greater than or equal to -16ºC?")
-							.minimumScaleFactor(0.8)
-						Spacer()
-						Button(action: {
-							fuelTemp.toggle()
-						}, label: {
-							Text(fuelTemp ? "Yes" : "No")
-						})
-						.frame(width: 100, height: 50, alignment: .center)
-						.background(RoundedRectangle(cornerRadius: 30).fill(fuelTemp ? Color.green : Color.red))
-						.foregroundColor(.white)
-					}
-					HStack {
-						Image(systemName:noVisibleMoisture ? "sun.max.fill" : "cloud.sleet.fill")
-							.renderingMode(.original)
-							.font(.largeTitle)
-						Text("Is Visible Moisture Present?")
-							.minimumScaleFactor(0.8)
-						Spacer()
-						Button(action: {
-							noVisibleMoisture.toggle()
-						}, label: {
-							Text(noVisibleMoisture ? "No" : "Yes")
-						})
-						.frame(width: 100, height: 50, alignment: .center)
-						.background(RoundedRectangle(cornerRadius: 30).fill(noVisibleMoisture ? Color.green : Color.red))
-						.foregroundColor(.white)
-					}
-					Text("rain, snow, drizzle, or fog with less than 1mi visibility")
-						.font(.footnote)
-						.foregroundColor(.gray)
-				}.padding([.top])
+				// OAT, Fuel Temp, Precip Conditions
+				ThreeConditionsView
 
+				Spacer()
 
 				VStack {
 					Button(action: {
 						evaluate()
 					}, label: {
-						ButtonView(title: "Evaluate", backgroundColor: .blue)
+						EvaluateButtonView(title: "Evaluate", backgroundColor: .blue)
 					})
 					Button(action: {
 						resetValues()
@@ -131,26 +75,73 @@ struct ContentView: View {
 				}
 				Spacer()
 			}
-			.padding([.leading, .trailing])
 			.font(.title2)
+			.padding([.leading, .trailing])
 
-			// Seahawks Blue background color.
-			.background(Color("BackgroundColor").edgesIgnoringSafeArea(.bottom)
-							.opacity(0.7)
-							.cornerRadius(30)
-							.padding([.leading, .trailing], 6)
-			)
-			.navigationBarTitle(Text("Deice 737?"))
-
-			.background(Image("clouds").resizable().edgesIgnoringSafeArea(.all))
+			.background(
+				ZStack {
+					Image("clouds").resizable().edgesIgnoringSafeArea(.all)
+					// Seahawks Blue background color.
+					Color("BackgroundColor").edgesIgnoringSafeArea(.bottom)
+						.opacity(0.7)
+						.cornerRadius(30)
+				})
 
 			.alert(isPresented: $showAlert, content: {
 				Alert(title: Text(alert?.title ?? "Error Occurred"), message: Text(alert?.description ?? ""), dismissButton: .default(Text("OK"), action: {
 					frostOutsideCSFF = false
 				}))
 			})
+			.navigationBarTitle(Text("Deice 737?"))
+		}
+		.colorScheme(.dark)
+	}
 
-		}.colorScheme(.dark)
+	private var ThreeConditionsView: some View {
+		Group {
+			HStack {
+				Image(systemName:outsideAirTemp ? "thermometer.sun.fill" : "thermometer.snowflake")
+					.renderingMode(.original)
+					.font(.largeTitle)
+				Text("Is OAT greater than or equal to +4ºC?")
+					.minimumScaleFactor(0.8)
+				Spacer()
+				Button(action: {
+					outsideAirTemp.toggle()
+				}, label: {
+					YesNoButtonView(title: outsideAirTemp ? "Yes" : "No", condition: outsideAirTemp)
+				})
+			}
+			HStack {
+				Image(systemName:fuelTemp ? "airplane.circle.fill" : "drop.triangle.fill")
+					.renderingMode(.original)
+					.font(.largeTitle)
+				Text("Is Fuel greater than or equal to -16ºC?")
+					.minimumScaleFactor(0.8)
+				Spacer()
+				Button(action: {
+					fuelTemp.toggle()
+				}, label: {
+					YesNoButtonView(title: fuelTemp ? "Yes" : "No", condition: fuelTemp)
+				})
+			}
+			HStack {
+				Image(systemName:noVisibleMoisture ? "sun.max.fill" : "cloud.sleet.fill")
+					.renderingMode(.original)
+					.font(.largeTitle)
+				Text("Is Visible Moisture Present?")
+					.minimumScaleFactor(0.8)
+				Spacer()
+				Button(action: {
+					noVisibleMoisture.toggle()
+				}, label: {
+					YesNoButtonView(title: noVisibleMoisture ? "No" : "Yes", condition: noVisibleMoisture)
+				})
+			}
+			Text("rain, snow, drizzle, or fog with less than 1mi visibility")
+				.font(.footnote)
+				.foregroundColor(.gray)
+		}.padding(.top)
 	}
 
 	private func resetValues() {
@@ -195,6 +186,8 @@ struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		Group {
 			ContentView()
+
+
 //				.previewDevice("iPhone SE")
 //			ContentView()
 //				.previewDevice("iPhone 12 Pro Max")
--- Deice737/CustomButtonViews.swift
+++ Deice737/CustomButtonViews.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ButtonView: View {
+struct EvaluateButtonView: View {
 	let title: String
 	let backgroundColor: Color
 
@@ -15,16 +15,28 @@ struct ButtonView: View {
 		Text(title)
 			.frame(maxWidth: 200, minHeight: 40)
 			.foregroundColor(.white)
-			.font(.title)
+//			.font(.title)
 			.background(RoundedRectangle(cornerRadius: 20).fill(backgroundColor))
 			.shadow(color: Color.black.opacity(0.6), radius: 7, x: 5, y: 5)
 			.padding()
     }
 }
 
+struct YesNoButtonView: View {
+	let title: String
+	let condition: Bool
+
+	var body: some View {
+		Text(title)
+			.frame(width: 100, height: 50)
+			.background(RoundedRectangle(cornerRadius: 30).fill(condition ? Color.green : Color.red))
+			.foregroundColor(.white)
+	}
+}
+
 
 struct ButtonView_Previews: PreviewProvider {
     static var previews: some View {
-		ButtonView(title: "Evaluate", backgroundColor: .blue)
+		EvaluateButtonView(title: "Evaluate", backgroundColor: .blue)
     }
 }
